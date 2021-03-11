8e8f8c69a86a663ab773907f6202d34d9ee72dd9
alexgerardojacinto <alexandre.jacinto@outsystems.com>
2021-03-11 16:58:00 +0000

    feat: add initial structure of the plugin
    References: https://outsystemsrd.atlassian.net/browse/RMET-547

--- src/android/com/outsystems/plugins/firebaseperformance/OSFirebasePerformance.java
+++ src/android/com/outsystems/plugins/firebaseperformance/OSFirebasePerformance.java
@@ -9,30 +9,55 @@ import org.json.JSONObject;
 
 public class OSFirebasePerformance extends CordovaPlugin {
 
+    private final static String KEY_ACTION_START_TRACE = "startTrace";
+    private final static String KEY_ACTION_STOP_TRACE = "stopTrace";
+    private final static String KEY_ACTION_ADD_TRACE_ATTRIBUTE = "addTraceAttribute";
+    private final static String KEY_ACTION_INCREMENT_METRIC = "incrementMetric";
+    private final static String KEY_ACTION_SET_PERFORMANCE_COLLECTION_ENABLED = "setPerformanceCollectionEnabled";
+
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-        if (action.equals("coolMethod")) {
-            String message = args.getString(0);
-            this.coolMethod(message, callbackContext);
+        if (action.equals(KEY_ACTION_START_TRACE)) {
+            this.startTrace(args, callbackContext);
+            return true;
+        }
+        else if (action.equals(KEY_ACTION_STOP_TRACE)) {
+            this.stopTrace(args, callbackContext);
+            return true;
+        }
+        else if (action.equals(KEY_ACTION_ADD_TRACE_ATTRIBUTE)) {
+            this.addTraceAttribute(args, callbackContext);
+            return true;
+        }
+        else if (action.equals(KEY_ACTION_INCREMENT_METRIC)) {
+            this.incrementMetric(args, callbackContext);
+            return true;
+        }
+        else if (action.equals(KEY_ACTION_SET_PERFORMANCE_COLLECTION_ENABLED)) {
+            this.setPerformanceCollectionEnabled(args, callbackContext);
             return true;
         }
         return false;
     }
 
-    private void coolMethod(String message, CallbackContext callbackContext) {
-        if (message != null && message.length() > 0) {
-            callbackContext.success(message);
-        } else {
-            callbackContext.error("Expected one non-empty string argument.");
-        }
+    private void startTrace(JSONArray args, CallbackContext callbackContext) {
+        //TODO
     }
 
-    protected String privateHelloWorldString() {
-        return "Hello World";
+    private void stopTrace(JSONArray args, CallbackContext callbackContext) {
+        //TODO
     }
 
-    public String publicHelloWorld() {
-        return this.privateHelloWorldString();
+    private void addTraceAttribute(JSONArray args, CallbackContext callbackContext) {
+        //TODO
+    }
+
+    private void incrementMetric(JSONArray args, CallbackContext callbackContext) {
+        //TODO
+    }
+
+    private void setPerformanceCollectionEnabled(JSONArray args, CallbackContext callbackContext) {
+        //TODO
     }
 
 }
