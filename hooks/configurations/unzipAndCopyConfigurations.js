000aa9446373d09ca7f67da6c17cccde907039bf
alexgerardojacinto <alexandre.jacinto@outsystems.com>
2021-03-29 19:37:39 +0100

    fix: do not copy file to destination folder in hook
    This is done by the <resource-file> tag that includes the .json and .plist google-services files in the analytics plugin.

--- hooks/configurations/unzipAndCopyConfigurations.js
+++ hooks/configurations/unzipAndCopyConfigurations.js
@@ -5,8 +5,6 @@ var AdmZip = require("adm-zip");
 
 var utils = require("./utilities");
 
-var utilsApp = require("../utilities");
-
 var constants = {
   googleServices: "google-services"
 };
@@ -55,33 +53,21 @@ module.exports = function(context) {
   var sourceFilePath = path.join(targetPath, fileName);
   var destFilePath = path.join(context.opts.plugin.dir, fileName);
 
-  var androidPath =  "platforms/android/app";
-  var iOSPath = "platforms/ios/" + utilsApp.getAppName(context) + "/Resources";
-
-  var completeFilePath;
-
-  var isAndroid = platform.localeCompare("android");
-  var isIOS = platform.localeCompare("ios");
-
-  if(isAndroid == 0){ //android code
-    completeFilePath = path.join(context.opts.projectRoot, androidPath);
-  }
-  else if(isIOS == 0){ //iOS code
-    completeFilePath = path.join(context.opts.projectRoot, iOSPath);
-  }
-
   if(!utils.checkIfFolderExists(destFilePath)){
     utils.copyFromSourceToDestPath(defer, sourceFilePath, destFilePath);
   }
 
+  /* I believe this is not necessary, because the files are already copied to the correct folder in the line above, so comment
   if (cordovaAbove7) {
-    if (utils.checkIfFolderExists(completeFilePath)) {
-      var destFilePath = path.join(completeFilePath, fileName);
-      if(!utils.checkIfFolderExists(destFilePath)){
+    var destPath = path.join(context.opts.projectRoot, "platforms", platform, "app");
+    if (utils.checkIfFolderExists(destPath)) {
+      var destFilePath = path.join(destPath, fileName);
+      if(!utils.checkIfFolderExists(destPath)){
         utils.copyFromSourceToDestPath(defer, sourceFilePath, destFilePath);
       }
     }
   }
+  */
       
   return defer.promise;
 }
