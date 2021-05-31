0a289013c751aa8e0040b30ecf1c41d1945c4b72
André Grillo <andregrillo@gmail.com>
2021-05-31 10:51:21 +0100

    Hooks folder removed

--- hooks/utilities.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  getAppName: function (context) {
-    var ConfigParser = context.requireCordovaModule("cordova-lib").configparser;
-    var config = new ConfigParser("config.xml");
-    return config.name();
-  }
-};
